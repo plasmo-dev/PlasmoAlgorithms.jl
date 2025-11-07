ac0cab5429834fc20edae9a55c2722ecb04cf2f8
David Cole <dlcdavidc@gmail.com>
2025-11-07 11:48:26 -0500

    Updated doc pages to handle different dependencies

--- docs/install.jl
+++ docs/install.jl
@@ -2,6 +2,16 @@ using Pkg
 
 PA_DIR = pwd()
 
-Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoBenders"))
-Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoSchwarz"))
+doc_pkg = get(ENV, "DOC_PKG", nothing)
+
+if doc_pkg === nothing
+	# Fallback: develop both (may conflict if dependencies are incompatible)
+	@info "DOC_PKG not set; developing both PlasmoBenders and PlasmoSchwarz" 
+	Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoBenders"))
+	Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoSchwarz"))
+else
+	@info "Developing only $(doc_pkg) for docs build"
+	Pkg.develop(path=joinpath(PA_DIR, "lib", doc_pkg))
+end
+
 Pkg.instantiate()
--- docs/make.jl
+++ docs/make.jl
@@ -2,40 +2,63 @@
 #  This Source Code Form is subject to the terms of the MIT License
 #  This source code is adapted from that of Plasmo.jl which can be found at https://github.com/plasmo-dev/Plasmo.jl/blob/main/docs/make.jl
 
-using Documenter, Plasmo, Suppressor, Graphs, PlasmoBenders, PlasmoSchwarz
+using Documenter, Plasmo, Suppressor, Graphs
+
+doc_pkg = get(ENV, "DOC_PKG", "PlasmoBenders")  # default to PlasmoBenders
+
+if doc_pkg == "PlasmoBenders"
+    @info "Building docs for PlasmoBenders"
+    using PlasmoBenders
+    DocMeta.setdocmeta!(PlasmoBenders, :DocTestSetup, :(using PlasmoBenders); recursive=true)
+elseif doc_pkg == "PlasmoSchwarz"
+    @info "Building docs for PlasmoSchwarz"
+    using PlasmoSchwarz
+    DocMeta.setdocmeta!(PlasmoSchwarz, :DocTestSetup, :(using PlasmoSchwarz); recursive=true)
+else
+    error("Unsupported DOC_PKG=$(doc_pkg). Expected PlasmoBenders or PlasmoSchwarz.")
+end
 
 DocMeta.setdocmeta!(Plasmo, :DocTestSetup, :(using Plasmo); recursive=true)
-DocMeta.setdocmeta!(PlasmoBenders, :DocTestSetup, :(using PlasmoBenders); recursive=true)
-DocMeta.setdocmeta!(PlasmoSchwarz, :DocTestSetup, :(using PlasmoSchwarz); recursive=true)
+DocMeta.setdocmeta!(Plasmo, :DocTestSetup, :(using Plasmo); recursive=true)
 
-makedocs(;
-    sitename="PlasmoAlgorithms.jl",
-    modules=[PlasmoBenders],
+pages_benders = [
+    "Introduction" => "index.md",
+    "PlasmoBenders.jl" => [
+        "Introduction" => "PlasmoBenders/introduction.md",
+        "Algorithm" => "PlasmoBenders/algorithm.md",
+        "Quickstart" => "PlasmoBenders/quickstart.md",
+        "Solver Options" => "PlasmoBenders/solver.md",
+        "Exploiting Graph Structure" => "PlasmoBenders/graph_structure.md",
+        "API Documentation" => "PlasmoBenders/api_docs.md",
+        "Tutorials" => [
+            "Storage Operation" => "PlasmoBenders/storage_tutorial.md",
+            "Equipment Sizing" => "PlasmoBenders/sizing_tutorial.md"
+        ],
+    ],
+]
+
+pages_schwarz = [
+    "Introduction" => "index.md",
+    "PlasmoSchwarz.jl" => [
+        "Introduction" => "PlasmoSchwarz/introduction.md",
+        "Quickstart" => "PlasmoSchwarz/quickstart.md",
+        "Algorithm" => "PlasmoSchwarz/algorithm.md",
+        "API Documentation" => "PlasmoSchwarz/api_docs.md",
+    ]
+]
+
+modules_sel = doc_pkg == "PlasmoBenders" ? [PlasmoBenders] : [PlasmoSchwarz]
+pages_sel = doc_pkg == "PlasmoBenders" ? pages_benders : pages_schwarz
+
+makedocs(; 
+    sitename="PlasmoAlgorithms.jl - $(doc_pkg)",
+    modules=modules_sel,
     doctest=true,
     checkdocs=:export,
     format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
     authors="Jordan Jalving and David Cole",
-    pages=[
-        "Introduction" => "index.md",
-        "PlasmoBenders.jl" => [
-            "Introduction" => "PlasmoBenders/introduction.md",
-            "Algorithm" => "PlasmoBenders/algorithm.md",
-            "Quickstart" => "PlasmoBenders/quickstart.md",
-            "Solver Options" => "PlasmoBenders/solver.md",
-            "Exploiting Graph Structure" => "PlasmoBenders/graph_structure.md",
-            "API Documentation" => "PlasmoBenders/api_docs.md",
-            "Tutorials" => [
-                "Storage Operation" => "PlasmoBenders/storage_tutorial.md",
-                "Equipment Sizing" => "PlasmoBenders/sizing_tutorial.md"
-            ],
-        ],
-        "PlasmoSchwarz.jl" => [
-            "Introduction" => "PlasmoSchwarz/introduction.md",
-            "Quickstart" => "PlasmoSchwarz/quickstart.md",
-            "Algorithm" => "PlasmoSchwarz/algorithm.md",
-            "API Documentation" => "PlasmoSchwarz/api_docs.md",
-        ]
-    ],
+    pages=pages_sel,
 )
 
-deploydocs(; repo="github.com/plasmo-dev/PlasmoAlgorithms.jl.git")
+devurl = doc_pkg == "PlasmoBenders" ? "benders" : "schwarz"
+deploydocs(; repo="github.com/plasmo-dev/PlasmoAlgorithms.jl.git", devurl=devurl)
