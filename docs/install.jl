937226fa5330ab04e664a633cb18a98e42ddfd47
David Cole <76058020+dlcole3@users.noreply.github.com>
2025-11-07 20:21:51 -0500

    Reverted to previous docs page

--- docs/install.jl
+++ docs/install.jl
@@ -2,16 +2,6 @@ using Pkg
 
 PA_DIR = pwd()
 
-doc_pkg = get(ENV, "DOC_PKG", nothing)
-
-if doc_pkg === nothing
-	# Fallback: develop both (may conflict if dependencies are incompatible)
-	@info "DOC_PKG not set; developing both PlasmoBenders and PlasmoSchwarz" 
-	Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoBenders"))
-	Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoSchwarz"))
-else
-	@info "Developing only $(doc_pkg) for docs build"
-	Pkg.develop(path=joinpath(PA_DIR, "lib", doc_pkg))
-end
-
+Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoBenders"))
+Pkg.develop(path=joinpath(PA_DIR, "lib", "PlasmoSchwarz"))
 Pkg.instantiate()
